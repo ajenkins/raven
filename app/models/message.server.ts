b6757869ba410bff914db215bcf11002e19c0729
AJ Jenkins <aj.jenkins123@gmail.com>
2024-06-23 17:38:18 -0400

    Deleted unused function

--- app/models/message.server.ts
+++ app/models/message.server.ts
@@ -2,22 +2,6 @@ import type { Message } from "@prisma/client";
 
 import { prisma } from "~/db.server";
 
-// export function getMessagesForChat({ id }: Pick<Chat, "id">) {
-//   return prisma.chat.findFirst({
-//     where: { id },
-//     include: {
-//       messages: {
-//         select: {
-//           id: true,
-//           body: true,
-//           sentAt: true,
-//         },
-//         orderBy: { sentAt: "asc" },
-//       },
-//     },
-//   });
-// }
-
 export function sendMessage({
   body,
   chatId,
